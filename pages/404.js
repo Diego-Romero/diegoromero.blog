63ae3fdffe1b6db10b789d728cd46dc57596b7ba
Diego Romero <dromero@snapchat.com>
2022-06-06 11:33:01 +0100

    added a test api route and 404 page

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Layout from "../layouts/layout/layout";
+
+
+export default function Custom404() {
+    return (
+        <Layout>
+            <h1>Page not found</h1>
+        </Layout>
+    )
+}
